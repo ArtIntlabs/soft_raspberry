69436237a6760b1a75851f7a3928bb406db36d1d
jubick1337 <Lytdybr99>
2021-02-10 14:45:23 +0500

    api update

--- send_files.py
+++ send_files.py
@@ -51,6 +51,13 @@ def encode_file(file):
     return encoded
 
 
+def get_date_time(file):
+    date = get_file_date(file)
+    timing = get_file_timing(file)
+    timing += '.0000Z'
+    return date + 'T' + timing
+
+
 def get_file_timing(file):
     timing = file.split('_')[3]
     return f'{timing[:2]}:{timing[2:4]}:{timing[4:6]}'
@@ -58,16 +65,19 @@ def get_file_timing(file):
 
 def get_file_date(file):
     date = file.split('_')[2]
-    return f'{date[6:8]}.{date[4:6]}.{date[:4]}'
-
+    day = date[6:8]
+    month = date[4:6]
+    year = date[:4]
+    # return f'{date[6:8]}.{date[4:6]}.{date[:4]}'
+    return f'{year}-{month}-{day}'
 
 def send_file(file, session_id):
     mac_address = 'b8:27:eb:af:a1:00'
     # mac_address = get_mac_address()
     while not internet_on():
         continue
-    request = {'stationMac': mac_address, 'audioFile': encode_file(file).decode('utf-8'), 'time': get_file_timing(file),
-               'data': get_file_date(file), 'sessionId': session_id, 'microType': '0'}
+    request = {'stationMac': mac_address, 'audioFile': encode_file(file).decode('utf-8'),
+               'startTime': get_date_time(file), 'sessionId': session_id, 'microType': 0}
     response = requests.post(ADD_AUDIO_INFO_URL, json=request)
 
     while response.status_code != requests.codes.OK:
@@ -75,20 +85,25 @@ def send_file(file, session_id):
 
 
 def main(args):
+    with open(LOG_FILE, 'w') as f:
+        pass
     while True:
-        with open(LOG_FILE, 'w') as f:
-            time_started = time.time()
-            session_id = start_session()
-            f.write('Got session id\n')
-            while time_started - time.time() < 24 * 60 * 60:
-                current_files = os.listdir(args['dir_result'])
+        time_started = time.time()
+        session_id = start_session()
+        with open(LOG_FILE, 'a') as f:
+            f.write(f'Got session id {session_id}\n')
+        while time_started - time.time() < 24 * 60 * 60:
+            current_files = os.listdir(args['dir_result'])
+            with open(LOG_FILE, 'a') as f:
                 f.write(f'Seeing files {current_files} at dir {args["dir_result"]}\n')
-                for file in current_files:
-                    send_file(args['dir_result'] + file, session_id)
+            for file in current_files:
+                send_file(args['dir_result'] + file, session_id)
+                with open(LOG_FILE, 'a') as f:
                     f.write(f'Sent file {file}\n')
-                    os.remove(args['dir_result'] + file)
+                os.remove(args['dir_result'] + file)
+                with open(LOG_FILE, 'a') as f:
                     f.write(f'Deleted file {file}\n')
-                time.sleep(50)
+            time.sleep(50)
 
 
 if __name__ == '__main__':
