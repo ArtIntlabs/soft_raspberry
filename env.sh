dc75ab4375e23f08b288b10ef27a84603ae29839
kompotiks <lifami40@gmail.com>
2021-05-11 22:22:25 +0300

    Init branch

---
+++ env.sh
@@ -0,0 +1,3 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install python3-pyaudio libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 -y
+pip install pyaudio
---
+++ record.py
@@ -0,0 +1,48 @@
+import pyaudio
+import wave
+import os
+
+CHUNK_SIZE = 1024
+FORMAT = pyaudio.paInt16
+RATE = 16000
+SIZE_WAV = 3600 # 3600
+CHANNELS = 1
+SAVE_DIR = 'data'
+
+
+def save_file(filename, frames, p):
+    wf = wave.open(os.path.join(SAVE_DIR, filename), 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+
+def record():
+    if not os.path.exists(SAVE_DIR):
+        os.mkdir(SAVE_DIR)
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE,
+                    input=True, output=True,
+                    frames_per_buffer=CHUNK_SIZE)
+
+    frames = []
+    counter = 0
+    counter_file = 0
+    size_one_file = int(RATE / CHUNK_SIZE * SIZE_WAV)
+    while True:
+        data = stream.read(CHUNK_SIZE)
+        frames.append(data)
+
+        if size_one_file < counter:
+            save_file(f'wav-{counter_file}.wav', frames, p)
+            counter_file += 1
+            counter = 0
+            frames = []
+        counter += 1
+
+
+if __name__ == '__main__':
+    record()
